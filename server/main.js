0ad914415d755f8048276693be3a840f6cb6ce2a
swahed <serjoscha.wahed@gmail.com>
2016-11-21 20:19:01 +0100

    Removed .tmp folder from git repo

--- server/main.js
+++ server/main.js
@@ -2,8 +2,9 @@ var express = require("express");
 
 var app = new express();
 
-app.get("/", function(req, res){
-	res.render("./../app/index.ejs");
-})
-.use(express.static(__dirname + "/../.tmp"))
-.listen(7777);
+app
+	.get("/", function(req, res){
+		res.render("./../app/index.ejs");
+	})
+	.use(express.static(__dirname + "/../.tmp"))
+	.listen(7777);
